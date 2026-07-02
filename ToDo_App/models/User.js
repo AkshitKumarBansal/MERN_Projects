2f5d8974162f5702316125b57c01c9c5b66afdae
Akshit Kumar Bansal <akbansal6204@gmail.com>
2026-07-02 15:15:43 +0530

    Add the Todo App

---
+++ ToDo_App/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+  firstName : String,
+  lastName : String,
+  userName : {type : String, required : true},
+  userPassword : {type : String, required : true},
+});
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if(!user.isModified) return next(); // check if the password is modified, if not, skip hashing
+  let salt = await bcrypt.genSalt(10); // generate a salt with 10 rounds
+  let hash = await bcrypt.hash(user.userPassword, salt); // hash the password with the generated salt
+  user.userPassword = hash;
+  next();
+});
+
+userSchema.methods.comparePassword = async function(password) {
+  return bcrypt.compare(password, this.password); // compare the provided password with the hashed password in the database
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
