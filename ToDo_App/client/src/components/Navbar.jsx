8bcd5219c27950fbd31b718d9a34c1b41e8381c8
Akshit Kumar Bansal <akbansal6204@gmail.com>
2026-07-03 14:27:05 +0530

    Design the Landing Page

---
+++ ToDo_App/client/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
+
+function Navbar({active}) {
+  return (
+    <header>
+      <nav>
+        <div className="logo__wrapper">
+          <img src={logo} alt="logo" />
+          <h4>ToDo!</h4>
+        </div>
+        <ul className="navigation-menu">
+          <li><a href="/" className={active==='home' && 'activeNav'}>Home</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/register">Register</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
+
--- ToDo_App/client/src/pages/Landing/Landing.jsx
+++ ToDo_App/client/src/pages/Landing/Landing.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
+import { Link } from 'react-router-dom'
+import landing from '../../assets/landing.jpg';
+import styles from './Landing.module.css';
 
-function landing() {
+function Landing() {
   return (
     <div>
-      landing page
+      <Navbar />
+      <div className={styles.landing__wrapper}>
+            <div className={styles.landing__text}>
+                <h1>Schedule Your Daily Tasks With <span className="primaryText">DoDo!</span></h1>
+            <div className='btnWrapper'>
+              <Link to="/register" className="primaryBtn">Register</Link>
+              <Link to="/login" className='secondaryBtn'>Login</Link>
+            </div>
+            </div>
+            <div className={styles.landing__img}>
+                <img src={landing} alt="landing" />
+            </div>
+      </div>
     </div>
   )
 }
 
-export default landing
+export default Landing
